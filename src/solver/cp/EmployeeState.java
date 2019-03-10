cf6cac4b44e44cd456e68ee0bc2160850d7a0baf
sandyharvie <csharvie16@gmail.com>
2019-03-10 15:36:19 -0400

    dang

--- src/solver/cp/EmployeeState.java
+++ src/solver/cp/EmployeeState.java
@@ -3,29 +3,30 @@ package solver.cp;
 import ilog.concert.*;
 
 public class EmployeeState {
-  IloIntVar working, shift, length;
+  IloIntVar shift, length;
 
   public EmployeeState(CPInstance instance, EmployeeState yesterday) throws IloException {
-    working = instance.cp.intVar(0, 1);
     shift = instance.cp.intVar(0, instance.numShifts-1);
-    length = instance.cp.intVar(instance.minConsecutiveWork, instance.maxDailyWork);
+    length = instance.cp.intVar(0, instance.maxDailyWork);
 
-    // (working == 0) <=> (shift == 0)
-    instance.cp.add(
-      instance.cp.equiv(
-        instance.cp.eq(working, 0),
-        instance.cp.eq(shift, 0)
-      )
+    // (shift == 0) <=> (length == 0)
+    instance.cp.add(instance.cp.equiv(
+      instance.cp.eq(shift, 0),
+      instance.cp.eq(length, 0))
     );
+
+    // (length > 0) => (length >= minConsecutiveWork)
+    instance.cp.add(instance.cp.imply(
+      instance.cp.gt(length, 0),
+      instance.cp.ge(length, instance.minConsecutiveWork)
+    ));
 
     // If yesterday's shift was the night shift, then today's shift cannot be
     if (yesterday != null) {
-      instance.cp.add(
-        instance.cp.imply(
-          instance.cp.eq(yesterday.shift, instance.numShifts-1),
-          instance.cp.neq(shift, instance.numShifts-1)
-        )
-      );
+      instance.cp.add(instance.cp.imply(
+        instance.cp.eq(yesterday.shift, instance.numShifts-1),
+        instance.cp.neq(shift, instance.numShifts-1)
+      ));
     }
   }
 }
